aa32b8eb165ea21d51185f104a56380d6af5896e
popastefan10 <popashtefan10@gmail.com>
2020-06-17 14:33:42 +0300

    Fixed snake movement

--- Snake Game SFML/Scripts/game.cpp
+++ Snake Game SFML/Scripts/game.cpp
@@ -10,6 +10,7 @@ game::~game() {
 
 void game::runGame() {
   gameWindow.create(sf::VideoMode(600, 600), "Game");
+  gameWindow.setFramerateLimit(500);
 
   Harta harta("harta.in");
   snake Snake(harta);
@@ -18,10 +19,32 @@ void game::runGame() {
     sf::Event event;
 
     while(gameWindow.pollEvent(event)) {
+
       if(event.type == sf::Event::Closed)
         gameWindow.close();
+
+      if(event.type == sf::Event::KeyPressed) {
+        int newDirection = 0;
+
+        if(event.key.code == sf::Keyboard::Up)
+          newDirection = 0;
+        else if(event.key.code == sf::Keyboard::Right)
+          newDirection = 1;
+        else if(event.key.code == sf::Keyboard::Down)
+          newDirection = 2;
+        else if(event.key.code == sf::Keyboard::Left)
+          newDirection = 3;
+        else if(event.key.code == sf::Keyboard::Escape)
+          gameWindow.close();
+
+        if(validDirections(Snake.getDirection(), newDirection))
+          Snake.setDirection(newDirection);
+      }
     }
 
+    Snake.moveBody();
+
+    gameWindow.clear();
     gameWindow.draw(Snake);
     gameWindow.display();
   }
